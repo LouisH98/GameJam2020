634bddedc8a931dbbe09fba6f6dd72a5114da16f
dg16066 <dg16066@essex.ac.uk>
2020-01-31 22:49:59 +0000

    player moves and beaver chases player. stops chasing when he gets close

---
+++ AI - Test/AI movement test/Assets/EnemyMovement.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+
+    public float Speed;
+    public float StoppingDistance;
+
+    private Transform _target;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _target = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Vector2.Distance(transform.position, _target.position) > StoppingDistance)
+        { 
+            transform.position = Vector2.MoveTowards(transform.position, _target.position, Speed * Time.deltaTime);
+        }
+
+    }
+}
---
+++ AI - Test/AI movement test/Assets/PlayerMovement.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float MoveSpeed = 5f;
+    public Rigidbody2D Rb;
+    public Animator Animat;
+
+    private Vector2 _movement;
+    // Update is called once per frame
+    void Update()
+    {
+        //user input
+
+        _movement.x = Input.GetAxisRaw("Horizontal");
+        _movement.y = Input.GetAxisRaw("Vertical");
+
+        Animat.SetFloat("Horizontal",_movement.x);
+        Animat.SetFloat("Vertical", _movement.y);
+        Animat.SetFloat("Speed",_movement.sqrMagnitude);
+    }
+
+    void FixedUpdate()
+    {
+        //player _movement
+
+        Rb.MovePosition(Rb.position + _movement * MoveSpeed * Time.fixedDeltaTime);
+
+
+    }
+}
